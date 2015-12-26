ce2c8c03a55d32958ccdcd8ed71e4469be50f7ef
Evhenii Holodniak <holodniak.e@gmail.com>
2015-12-26 13:23:16 +0200

    added works section with owl carousel

--- app/js/common.js
+++ app/js/common.js
@@ -25,6 +25,17 @@ $(document).ready(function() {
 		$('.mobnav').fadeToggle();
 	});
 
+	$("#owl-1, #owl-2").owlCarousel({
+
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 6,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+
+  });
+
+
 	//Check
 	console.log('Done!');
 
